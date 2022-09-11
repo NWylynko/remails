55ccce5e921e776836668fb4b58fd4a471c4201e
Nick Wylynko <nick@wylynko.com>
2022-09-11 12:30:38 +0800

    export out test providers

--- src/index.ts
+++ src/index.ts
@@ -5,31 +5,8 @@ export { getEmail } from './getEmail';
 
 export { EmailProviders } from "./Providers";
 export { SendGrid } from "./providers/sendgrid";
+export { Success } from "./providers/successful";
+export { Fail } from "./providers/fail";
 
 export { useDetails } from "./components/context/Details"
 export type { Details } from "./components/context/Details"
-
-// const to = "nick@wylynko.com"
-// const from = "nick1014375@gmail.com"
-// const template = "bruh/bruhEmail"
-
-// const main = async () => {
-
-//   const apiKey = process.env.SENDGRID_API_KEY;
-
-//   if (!apiKey) {
-//     throw new Error(`the environment variable SENDGRID_API_KEY needs to be set`)
-//   }
-
-//   const { send } = EmailProviders([
-//     SendGrid({ apiKey })
-//   ])
-
-//   const email = await getEmail({ template, to, from })
-
-//   await send(email)
-
-//   console.log(email)
-// }
-
-// main()
